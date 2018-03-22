94adee245c37152449b2f45617d0405079625452
Jayson Nabor <sonabstudios@gmail.com>
2018-03-22 23:08:02 +0800

    updated sample sam

--- dynamodb-create-table/index.js
+++ dynamodb-create-table/index.js
@@ -1,8 +1,4 @@
-'use strict';
-
-console.log('Loading function');
-
 exports.handler = (event, context, callback) => {
     // TODO implement
-    callback(null, 'Hello from Lambda');
-};
+    console.log("test")
+}
